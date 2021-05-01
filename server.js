89035deb42134b0c42133db3058fb0c056a82c45
FareedKandalaft <fareed@spectrecoder.com>
2021-05-01 16:39:38 -0400

    Config files and db.js for exported connectDB()

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
-
 const app = express();
+const connectDB = require('./config/db');
+
+// Connect to DB
+connectDB();
 
 app.get('/', (req, res) => {
   res.json({ msg: 'Welcome to the ContactKepper API' });
