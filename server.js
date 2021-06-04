50a97ee260ecc75bc040b234b74bb00988d89c0d
FareedKandalaft <fareed@spectrecoder.com>
2021-06-04 17:38:24 -0400

    Prepare for Production release to Heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const connectDB = require('./config/db');
+const path = require('path');
 
 // Connect to DB
 connectDB();
@@ -8,17 +9,27 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => {
-  res.json({ msg: 'Welcome to the ContactKepper API' });
-});
+// app.get('/', (req, res) => {
+//   res.json({ msg: 'Welcome to the ContactKepper API' });
+// });
+
+// DEFINE ROUTES
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
+
+// Serve static assets in production
+// **IMPORTANT** this if statement is below the DEFINE ROUTES section
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
 });
-
-// DEFINE ROUTES
-app.use('/api/users', require('./routes/users'));
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/contacts', require('./routes/contacts'));
